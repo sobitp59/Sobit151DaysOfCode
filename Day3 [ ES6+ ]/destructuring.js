79b5f295181e90bdba1e5c4cf57b1f34ebc47e71
sobitp59 <sobitp59@gmail.com>
2023-02-13 19:38:09 +0530

    learn alias

--- Day3 [ ES6+ ]/destructuring.js
+++ Day3 [ ES6+ ]/destructuring.js
@@ -25,9 +25,29 @@ const {fullName, isStudent, degree} = student
 console.log(fullName, isStudent, degree)
 
 
-// 03 
+// 03 : destructuring to pass arguments in a function
 const destructExample = ({fullName, isStudent, degree}) => {
     console.log(fullName, isStudent, degree)
 }
 
 destructExample(student)
+
+
+// 04 : giving alias(other name) to a object property
+const animeCharacters = {
+    characterOne : 'Naruto Uzumaki',
+    characterTwo : 'Sasuke Uchiha',
+    characterThree : 'Itachi Uchiha',
+    characterFour : 'Kakashi Hatake'
+}
+
+const animeCharacterNames = ({characterOne : naruto, 
+                              characterTwo : sasuke, 
+                              characterThree : itachi,
+                              characterFour : kakashi  
+                            }) => {
+            console.log(naruto, sasuke, itachi, kakashi)
+            // console.log(characterOne, sasuke, itachi, kakashi) ReferenceError: characterOne is not defined
+}
+
+animeCharacterNames(animeCharacters)
