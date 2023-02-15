d4deb5491701e48f80dbf444786e3c269a6a2e0f
sobitp59 <sobitp59@gmail.com>
2023-02-15 23:23:52 +0530

    learn template literals

---
+++ Day5 [ ES6+ ]/templateLiterals.js
@@ -0,0 +1,26 @@
+// TEMPLATE LITERALS
+// Template literals are a feature in JavaScript that allow
+// you to embed expressions inside of string literals using
+//  backticks (` `) instead of single quotes (' ') or double quotes (" ").
+
+
+// 01
+const name = 'Sobit Prasad'
+const degree = 'BCA'
+
+console.log(`hey! my name is ${name} and i am in ${degree} final year.`)
+// hey! my name is Sobit Prasad and i am in BCA final year.
+
+
+// 02 : Multiple line string
+const poem = `Roses are red,
+Violets are blue,
+      Sugar is sweet,
+And so are you.`;
+
+console.log(poem);
+// OUTPUT : 
+// Roses are red,
+// Violets are blue,
+//       Sugar is sweet,
+// And so are you.
