696d7db4e38738fe8b0090469f2e2e8c6af51974
sobitp59 <sobitp59@gmail.com>
2023-02-15 23:34:13 +0530

    learn template literals

--- Day5 [ ES6+ ]/templateLiterals.js
+++ Day5 [ ES6+ ]/templateLiterals.js
@@ -24,3 +24,16 @@ console.log(poem);
 // Violets are blue,
 //       Sugar is sweet,
 // And so are you.
+
+
+// 03
+const evenOrOdd = (num) => {
+      if(!(num % 2)){
+            return `${num} is Even`
+      }
+      return `${num} is Odd`
+}
+console.log(evenOrOdd(151)) // 151 is Odd
+console.log(evenOrOdd(1))   // 1 is Odd
+console.log(evenOrOdd(22))  // 22 is Even
+console.log(evenOrOdd(77))  // 77 is Odd
