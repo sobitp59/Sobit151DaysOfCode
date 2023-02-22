8d3d96e0eaee6a7cb8227b857905cb80898e1964
sobitp59 <sobitp59@gmail.com>
2023-02-22 13:29:26 +0530

    completed es6+ of freecodecamp

--- Day11-12 [ ES6+ freeCodeCamp]/q22.js
+++ Day11-12 [ ES6+ freeCodeCamp]/q22.js
@@ -1,1 +1,17 @@
 // Promises
+
+const makeServerRequest = new Promise((resolve, reject) => {
+    const connection = true;
+    if(connection){
+        resolve("data successfully fetched!")
+    }else{
+        reject("failed to fetch data!")
+    }
+})
+
+makeServerRequest.then(result => {
+    console.log(result)
+})
+.catch(error => {
+    console.log(error)
+})
